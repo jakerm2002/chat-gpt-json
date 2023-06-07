fb089711f91fffeabb82b071ab9dca73a4d4a7cc
Jake Medina <jake.medina02@gmail.com>
2023-06-07 16:31:45 -0500

    code cleanup

--- script.py
+++ script.py
@@ -5,15 +5,17 @@ import operator
 import sys
 import os
 
+
 def getAuthorString(chat):
     authorInfo = chat['message']['author']['role']
-    return "user" if authorInfo == "user" else "GPT" if authorInfo == "assistant" else "SYSTEM_ROOT"
+    return "user" if authorInfo == "user" else "GPT" if authorInfo == "assistant" else "SYSTEM"
+
 
 def printFormat(mapping, node_id, level):
     num_spaces = level * 2
     authorString = getAuthorString(mapping[node_id])
-    # print((" " * num_spaces) + " - " + messageAuthor + node_id)
     print(f"{' ' * num_spaces}- {authorString} {node_id}")
+
 
 # root_id: id field of the root node
 def depth_first(mapping, node_id, index, level, write, writer, feedback):
@@ -33,24 +35,22 @@ def get_system_node_id(conversation):
     system_node_id = None
     for chat in conversation['mapping'].values():
         try:
-            # print(chat['message']['author']['role'])
             if chat['message']['author']['role'] == "system":
                 system_node_id = chat['id']
         except:
             pass
-    
     if not system_node_id:
-        raise ValueError('System node (root) not found in conversation data. Program does not know the root and cannot proceed.')
-
-    # print(system_node_id)
+        raise ValueError('System node not found in conversation data. Program does not know the children and cannot proceed.')
     return system_node_id
-    # iterCount = 0
+
 
 def get_level_indicator(level):
     num_spaces = level * 2
     # return f"{'x' * level}"
     return f"{'  ' * num_spaces}x"
 
+
+csv_header_columns = ['message_id', 'author', 'level_indicator', 'lvl', 'create_time', 'rating', 'tags', 'text_feedback', 'is_original_message', 'message_contents', 'parent', 'children']
 def write_message_to_csv(writer, chat, feedbackObject, level, index):
     message_contents = [part for part in chat['message']['content']['parts']] if len(chat['message']['content']['parts']) > 1 else chat['message']['content']['parts'][0]
     create_time = get_UTC_timestamp(chat['message']['create_time'])
@@ -68,10 +68,10 @@ def write_message_to_csv(writer, chat, feedbackObject, level, index):
 
     writer.writerow([chat['id'], author, level_indicator, level, create_time, rating, tags, text_feedback, is_original_message, message_contents, parent, children])
 
+
 def get_UTC_timestamp(epoch_time):
     return datetime.datetime.utcfromtimestamp(epoch_time).strftime('%Y-%m-%d %H:%M:%S')
 
-csv_header_columns = ['message_id', 'author', 'level_indicator', 'lvl', 'create_time', 'rating', 'tags', 'text_feedback', 'is_original_message', 'message_contents', 'parent', 'children']
 
 # returns a dictionary containing the feedback for this conversation
 # key: a message id
@@ -83,9 +83,11 @@ def get_conversation_feedback(conversation_id):
             feedbackDict[feedback['id']] = feedback
     return feedbackDict
 
+
 # removes backslashes and dots to avoid python open() command when writing
 def format_output_conversation_title(conversation_title):
     return conversation_title.replace("/", "-")
+
 
 def deserialize(folder_path):
     global conversationsJSON
@@ -98,6 +100,7 @@ def deserialize(folder_path):
     messageFeedbackFile = open(f'{folder_path}/message_feedback.json')
     messageFeedbackJSON = json.load(messageFeedbackFile)
     messageFeedbackJSON = sorted(messageFeedbackJSON, key=operator.itemgetter('create_time'))
+
 
 def main(folder_path):
     # Your main program logic here
@@ -122,17 +125,13 @@ def main(folder_path):
         print(printTitle)
         system_node_id = get_system_node_id(conversation)
 
-        # open the file in the write mode
         with open(csvTitle, 'w') as f:
-            # create the csv writer
             writer = csv.writer(f)
-
             writer.writerow(csv_header_columns)
-            # write a row to the csv file
-            # writer.writerow(["Hello"])
             depth_first(conversation['mapping'], system_node_id, 0, 0, write_message_to_csv, writer, feedback)
 
         print()
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
